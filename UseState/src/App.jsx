c947d767b6854f3b846244fc6dfd3d9be940f039
Hit1912 <hitdungrani39@gmail.com>
2026-03-14 11:35:57 +0530

    UseState Update

--- UseState/src/App.jsx
+++ UseState/src/App.jsx
@@ -2,33 +2,72 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [number, setnumber ] = useState(0);
-  // Number ==> Read Only
-  //SetNumber ==> Write Only (jo number ni value chagne karvi hoy srtnumber no use karvo)
+  const [number, setnumber] = useState(0);
+
   function increaseNum(){
- setnumber(number + 1)                               // 2.Method ==>  let newnumber = number + 1 ;
-  // Command This code   setnumber(newnumber); 
-}
+    setnumber(number + 1);
+  }
 
   function decreaseNum(){
-    setnumber(number - 1)
-  // 2.Method ==>   let newnumber = number - 1 ;
-// Command This code   setnumber(newnumber);    
-}
+    if(number > 0){
+      setnumber(number - 1);
+    }
+  }
+
+  function jumpIncrease(){
+    setnumber(number + 5);
+  }
+
+  function jumpDecrease(){
+    if(number >= 5){
+      setnumber(number - 5);
+    }else{
+      setnumber(0);
+    }
+  }
 
   return (
    <>
       <section className="py-15">
-        <h1 className="text-4xl text-center font-extrabold">UseState == Data That Can Change (Data ne Change Karvo)</h1>
-      <p className="text-2xl py-3 font-bold text-center">Increase And Decrease Counter</p>
-      <div className="w-52 h-52 border bg-[#555] text-5xl font-extrabold rounded-xl py-2 px-4 flex 
-      items-center justify-center text-[#222] mx-auto">
-        {number}
-      </div >
-      <div className="flex justify-center items-center mt-5 gap-5">
-            <button className="bg-[#555] px-4 py-1 text-center rounded-lg w-54 h-12 text-xl " onClick={increaseNum}>Increase</button>
-      <button className="bg-[#555] px-4 py-1 text-center rounded-lg w-54 h-12 text-xl " onClick={decreaseNum}>Decrease</button>
-      </div>
+        <h1 className="text-4xl text-center font-extrabold">
+          Counter With Jump
+        </h1>
+
+        <div className="w-52 h-52 border bg-[#555] text-5xl font-extrabold rounded-xl py-2 px-4 flex 
+        items-center justify-center text-[#222] mx-auto">
+          {number}
+        </div>
+
+        <div className="flex justify-center items-center mt-5 gap-5 flex-wrap">
+
+        <button 
+className="bg-linear-to-r from-green-400 to-green-600 hover:scale-105 hover:shadow-lg 
+text-white px-6 py-2 rounded-xl font-semibold transition duration-300"
+onClick={increaseNum}>
+Increase +1
+</button>
+
+<button 
+className="bg-linear-to-r from-red-400 to-red-600 hover:scale-105 hover:shadow-lg 
+text-white px-6 py-2 rounded-xl font-semibold transition duration-300"
+onClick={decreaseNum}>
+Decrease -1
+</button>
+
+<button 
+className="bg-linear-to-r from-blue-400 to-blue-600 hover:scale-105 hover:shadow-lg 
+text-white px-6 py-2 rounded-xl font-semibold transition duration-300"
+onClick={jumpIncrease}>
+Jump +5
+</button>
+
+<button 
+className="bg-linear-to-r from-purple-400 to-purple-600 hover:scale-105 hover:shadow-lg 
+text-white px-6 py-2 rounded-xl font-semibold transition duration-300"
+onClick={jumpDecrease}>
+Jump -5
+</button>
+        </div>
       </section>
    </>
   )
