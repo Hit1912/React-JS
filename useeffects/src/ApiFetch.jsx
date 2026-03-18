24fa190b6928f162b380b9f4ceb22fc18f8d516f
Hit1912 <hitdungrani39@gmail.com>
2026-03-18 12:53:15 +0530

    usestate

---
+++ useeffects/src/ApiFetch.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+const ApiFetch = () => {
+  const [Data1, setData1] = useState([]);
+  const [Data2, setData2] = useState([]);
+  const [Data3, setData3] = useState([]);
+
+  // method 1
+  const method1 = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((rawdata) => {
+        console.log(rawdata);
+        return rawdata.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return setData1(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // method 2(async, await , fetch)
+  const method2 = async () => {
+    try {
+      let response =await fetch("https://dummyjson.com/products?limit=10");
+      let data =await response.json();
+      console.log(data.products);
+      return setData2(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // method 3
+
+  return (
+    <>
+      {/* method : 1 */}
+      <section className="bg-green-100 w-full h-screen p-6 flex flex-col justify-center items-center gap-y-4">
+        <h1 className="text-3xl">method 1: Fetch - then - then</h1>
+        <div className="bg-black text-white h-144 w-full rounded-md flex justify-center items-center gap-4 flex-wrap p-4 overflow-y-scroll">
+          {Data1.map((user, idx) => {
+            return (
+              <div key={idx} className="w-sm rounded-xl bg-[#222] p-2">
+                <p>
+                  <span className="font-bold">Name : </span>
+                  {user.name}
+                </p>
+                <p>
+                  <span className="font-bold">Email : </span>
+                  {user.email}
+                </p>
+                <p>
+                  <span className="font-bold">Website : </span>
+                  {user.website}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="px-4 py-2 text-center rounded-md bg-green-950 w-36 active:bg-red-100 active:border-green-950 active:text-green-950 active:scale-90 text-white focus:scale-95 font-bold"
+          onClick={method1}
+        >
+          Get data
+        </button>
+      </section>
+
+      {/* method : 2 */}
+      <section className="bg-blue-100 w-full h-screen p-6 flex flex-col justify-center items-center gap-y-4">
+        <h1 className="text-3xl">method 2: Async Await</h1>
+        <div className="bg-black h-144 w-full rounded-md flex flex-wrap items-center justify-center gap-4 overflow-auto p-4">
+        {Data2.map((products, idx) => {
+            return (
+              <div key={idx} className="w-72 rounded-md bg-[#222] p-4 text-white relative">
+                <img src={products.images[0]} className="w-full h-auto rounded-2xl bg-[#555]" alt="images" />
+                <div className="flex items-center justify-between p-4 ">
+                    <h1 className="text-sm font-bold">{products.title}</h1>
+                    <h1 className="text-nowrap bg-[#555] p-2 rounded-md">${products.price}</h1>
+                </div>
+                <p className="bg-orange-400 font-bold absolute p-1 px-3 top-4 rounded-tr-md rounded-br-md rounded-tl-xl left-4">{products.brand}</p>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="px-4 py-2 text-center rounded-md bg-blue-950 w-36 active:bg-blue-100 active:border-blue-950  active:text-blue-950 active:scale-90 text-white focus:scale-95 font-bold"
+          onClick={method2}
+        >
+          Get data
+        </button>
+      </section>
+
+      <section className="bg-red-100 w-full h-screen p-6 flex flex-col justify-center items-center gap-y-4">
+        <h1 className="text-3xl">method 3: Axios(Async Await)</h1>
+        <div className="bg-black h-144 w-full rounded-md "></div>
+        <button
+          className="px-4 py-2 text-center rounded-md bg-red-950 w-36 active:bg-red-100 active:border-red-950  active:text-red-950 active:scale-90 text-white focus:scale-95 font-bold"
+          onClick={method1}
+        >
+          Get data
+        </button>
+      </section>
+    </>
+  );
+};
+
+export default ApiFetch;
+ 
---
+++ useeffects/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApiFetch from './ApiFetch'
+
+const App = () => {
+  return (
+   <>
+   <ApiFetch />
+   </>
+  )
+}
+
+export default App
